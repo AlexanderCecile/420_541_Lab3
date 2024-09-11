4d2edc9c07af0195fa3f6f8efe43bfaef4c6aed6
AlexC <35971201+AlexanderCecile@users.noreply.github.com>
2024-09-11 12:53:28 -0400

    Fixed targets and adjusted hitboxes, completed part 6

--- Assets/Script/TargetComponent.cs
+++ Assets/Script/TargetComponent.cs
@@ -6,13 +6,18 @@ public class TargetComponent : MonoBehaviour
 {
 	public void OnCollisionEnter(Collision collision)
     {
-		Debug.Log("In OnCollisionEnter");
         if (collision.gameObject.CompareTag("Projectile"))
         {
             GameManager.Instance.IncrementScore();
-            
-            // Hide target after 5 seconds
-            Invoke("HideTarget", 5f);
+            Renderer parentRenderer = transform.parent.GetComponent<Renderer>();
+			if (parentRenderer != null)
+			{
+				parentRenderer.material.color = Color.green;
+				
+				Invoke("ChangeColorBack", 3f); // Change color back after 5 seconds
+			}
+
+			
         }
     }
 
@@ -20,4 +25,13 @@ public class TargetComponent : MonoBehaviour
     {
         gameObject.SetActive(false);
     }
+	
+	void ChangeColorBack()
+	{
+		Renderer parentRenderer = transform.parent.GetComponent<Renderer>();
+		if (parentRenderer != null)
+		{
+			parentRenderer.material.color = Color.red;
+		}
+	}
 }
